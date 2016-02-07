174fb027d5d5db2adcdaadf8a5772fcafdb38f4d
Steven <steventhon94@gmail.com>
2016-02-07 11:45:55 -0800

    Test alert onchange

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-function menuChange(id) {
-  if (document.getElementById('list'+id).style.display == 'none')
-    document.getElementById('list'+id).style.display = 'block';
-  else
-    document.getElementById('list'+id).style.display = 'none';
+//var manufacturer = "";
+//var state = "";
+
+function setManufacturer() {
+	alert('asdf');
+	//var m = document.getElementById("manufacturer");
+	//manufacturer = m.options[m.selectedIndex].value;
+  //alert("You have selected Product Manufacturer: " + manufacturer);
 }
+/*
+function menuChange(id) {
+  if (document.getElementById('list' + id).style.display == 'none')
+    document.getElementById('list' + id).style.display = 'block';
+  else
+    document.getElementById('list' + id).style.display = 'none';
+}
+
